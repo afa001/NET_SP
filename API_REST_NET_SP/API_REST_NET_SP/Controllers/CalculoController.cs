bb8b24a7e49fc3a485897b6f18a90a1aa9c996f8
afa001 <anderson.alarcon.afa@gmail.com>
2020-07-25 18:42:41 -0500

    api rest terminado, controladores, modelo entity framework

---
+++ API_REST_NET_SP/API_REST_NET_SP/Controllers/CalculoController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using API_REST_NET_SP.Models;
+
+namespace API_REST_NET_SP.Controllers
+{
+    public class CalculoController : ApiController
+    {
+        private bdIndraEntities1 db = new bdIndraEntities1();
+
+        // GET: api/Calculo
+        public IHttpActionResult GetCalculo()
+        {
+            //sp
+            var emp = db.GetCalculo().ToList();
+            return Ok(emp);
+        }
+
+        // GET: api/Calculo/5
+        [ResponseType(typeof(Calculo))]
+        public IHttpActionResult GetCalculo(int id)
+        {
+            var emp = db.GetIdCalculo(id);
+            if (emp == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(emp);
+        }
+
+        // PUT: api/Calculo/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutCalculo(int id, Calculo calculo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != calculo.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(calculo).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CalculoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Calculo
+        [ResponseType(typeof(Calculo))]
+        [HttpPost]
+        public IHttpActionResult PostCalculo(Calculo calculo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var calculoId = db.InsertCalculo(calculo.Respuesta,calculo.FechaHora,calculo.UsuarioId);
+
+            //db.Calculo.Add(calculo);
+            //db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = calculo.Id }, calculo);
+        }
+
+        // DELETE: api/Calculo/5
+        [ResponseType(typeof(Calculo))]
+        [HttpDelete]
+        public IHttpActionResult DeleteCalculo(int id)
+        {
+            /*var calculo = db.GetIdCalculo(id);
+            if (calculo == null)
+            {
+                return NotFound();
+            }*/
+
+            var emp = db.DeleteCalculo(id);
+            //db.Calculo.Remove(calculo);
+            //db.SaveChanges();
+
+            return Ok(emp);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool CalculoExists(int id)
+        {
+            return db.Calculo.Count(e => e.Id == id) > 0;
+        }
+    }
+}
--- API_REST_NET_SP/API_REST_NET_SP/Models/Model1.Context.cs
+++ API_REST_NET_SP/API_REST_NET_SP/Models/Model1.Context.cs
@@ -12,11 +12,13 @@ namespace API_REST_NET_SP.Models
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Infrastructure;
+    using System.Data.Entity.Core.Objects;
+    using System.Linq;
     
-    public partial class bdIndraEntities : DbContext
+    public partial class bdIndraEntities1 : DbContext
     {
-        public bdIndraEntities()
-            : base("name=bdIndraEntities")
+        public bdIndraEntities1()
+            : base("name=bdIndraEntities1")
         {
         }
     
@@ -28,5 +30,170 @@ namespace API_REST_NET_SP.Models
         public virtual DbSet<Calculo> Calculo { get; set; }
         public virtual DbSet<sysdiagrams> sysdiagrams { get; set; }
         public virtual DbSet<Usuario> Usuario { get; set; }
+    
+        public virtual int DeleteCalculo(Nullable<int> id)
+        {
+            var idParameter = id.HasValue ?
+                new ObjectParameter("Id", id) :
+                new ObjectParameter("Id", typeof(int));
+    
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction("DeleteCalculo", idParameter);
+        }
+    
+        public virtual ObjectResult<GetCalculo_Result> GetCalculo()
+        {
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction<GetCalculo_Result>("GetCalculo");
+        }
+    
+        public virtual ObjectResult<GetFilter_Result> GetFilter(Nullable<int> usuario, Nullable<int> respuestaMinima, Nullable<int> respuestaMaxima)
+        {
+            var usuarioParameter = usuario.HasValue ?
+                new ObjectParameter("Usuario", usuario) :
+                new ObjectParameter("Usuario", typeof(int));
+    
+            var respuestaMinimaParameter = respuestaMinima.HasValue ?
+                new ObjectParameter("RespuestaMinima", respuestaMinima) :
+                new ObjectParameter("RespuestaMinima", typeof(int));
+    
+            var respuestaMaximaParameter = respuestaMaxima.HasValue ?
+                new ObjectParameter("RespuestaMaxima", respuestaMaxima) :
+                new ObjectParameter("RespuestaMaxima", typeof(int));
+    
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction<GetFilter_Result>("GetFilter", usuarioParameter, respuestaMinimaParameter, respuestaMaximaParameter);
+        }
+    
+        public virtual ObjectResult<GetIdCalculo_Result> GetIdCalculo(Nullable<int> id)
+        {
+            var idParameter = id.HasValue ?
+                new ObjectParameter("Id", id) :
+                new ObjectParameter("Id", typeof(int));
+    
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction<GetIdCalculo_Result>("GetIdCalculo", idParameter);
+        }
+    
+        public virtual ObjectResult<GetUsuario_Result> GetUsuario()
+        {
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction<GetUsuario_Result>("GetUsuario");
+        }
+    
+        public virtual int InsertCalculo(Nullable<int> respuesta, Nullable<System.DateTime> fechaHora, Nullable<int> usuarioId)
+        {
+            var respuestaParameter = respuesta.HasValue ?
+                new ObjectParameter("Respuesta", respuesta) :
+                new ObjectParameter("Respuesta", typeof(int));
+    
+            var fechaHoraParameter = fechaHora.HasValue ?
+                new ObjectParameter("FechaHora", fechaHora) :
+                new ObjectParameter("FechaHora", typeof(System.DateTime));
+    
+            var usuarioIdParameter = usuarioId.HasValue ?
+                new ObjectParameter("UsuarioId", usuarioId) :
+                new ObjectParameter("UsuarioId", typeof(int));
+    
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction("InsertCalculo", respuestaParameter, fechaHoraParameter, usuarioIdParameter);
+        }
+    
+        public virtual int sp_alterdiagram(string diagramname, Nullable<int> owner_id, Nullable<int> version, byte[] definition)
+        {
+            var diagramnameParameter = diagramname != null ?
+                new ObjectParameter("diagramname", diagramname) :
+                new ObjectParameter("diagramname", typeof(string));
+    
+            var owner_idParameter = owner_id.HasValue ?
+                new ObjectParameter("owner_id", owner_id) :
+                new ObjectParameter("owner_id", typeof(int));
+    
+            var versionParameter = version.HasValue ?
+                new ObjectParameter("version", version) :
+                new ObjectParameter("version", typeof(int));
+    
+            var definitionParameter = definition != null ?
+                new ObjectParameter("definition", definition) :
+                new ObjectParameter("definition", typeof(byte[]));
+    
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction("sp_alterdiagram", diagramnameParameter, owner_idParameter, versionParameter, definitionParameter);
+        }
+    
+        public virtual int sp_creatediagram(string diagramname, Nullable<int> owner_id, Nullable<int> version, byte[] definition)
+        {
+            var diagramnameParameter = diagramname != null ?
+                new ObjectParameter("diagramname", diagramname) :
+                new ObjectParameter("diagramname", typeof(string));
+    
+            var owner_idParameter = owner_id.HasValue ?
+                new ObjectParameter("owner_id", owner_id) :
+                new ObjectParameter("owner_id", typeof(int));
+    
+            var versionParameter = version.HasValue ?
+                new ObjectParameter("version", version) :
+                new ObjectParameter("version", typeof(int));
+    
+            var definitionParameter = definition != null ?
+                new ObjectParameter("definition", definition) :
+                new ObjectParameter("definition", typeof(byte[]));
+    
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction("sp_creatediagram", diagramnameParameter, owner_idParameter, versionParameter, definitionParameter);
+        }
+    
+        public virtual int sp_dropdiagram(string diagramname, Nullable<int> owner_id)
+        {
+            var diagramnameParameter = diagramname != null ?
+                new ObjectParameter("diagramname", diagramname) :
+                new ObjectParameter("diagramname", typeof(string));
+    
+            var owner_idParameter = owner_id.HasValue ?
+                new ObjectParameter("owner_id", owner_id) :
+                new ObjectParameter("owner_id", typeof(int));
+    
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction("sp_dropdiagram", diagramnameParameter, owner_idParameter);
+        }
+    
+        public virtual ObjectResult<sp_helpdiagramdefinition_Result> sp_helpdiagramdefinition(string diagramname, Nullable<int> owner_id)
+        {
+            var diagramnameParameter = diagramname != null ?
+                new ObjectParameter("diagramname", diagramname) :
+                new ObjectParameter("diagramname", typeof(string));
+    
+            var owner_idParameter = owner_id.HasValue ?
+                new ObjectParameter("owner_id", owner_id) :
+                new ObjectParameter("owner_id", typeof(int));
+    
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction<sp_helpdiagramdefinition_Result>("sp_helpdiagramdefinition", diagramnameParameter, owner_idParameter);
+        }
+    
+        public virtual ObjectResult<sp_helpdiagrams_Result> sp_helpdiagrams(string diagramname, Nullable<int> owner_id)
+        {
+            var diagramnameParameter = diagramname != null ?
+                new ObjectParameter("diagramname", diagramname) :
+                new ObjectParameter("diagramname", typeof(string));
+    
+            var owner_idParameter = owner_id.HasValue ?
+                new ObjectParameter("owner_id", owner_id) :
+                new ObjectParameter("owner_id", typeof(int));
+    
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction<sp_helpdiagrams_Result>("sp_helpdiagrams", diagramnameParameter, owner_idParameter);
+        }
+    
+        public virtual int sp_renamediagram(string diagramname, Nullable<int> owner_id, string new_diagramname)
+        {
+            var diagramnameParameter = diagramname != null ?
+                new ObjectParameter("diagramname", diagramname) :
+                new ObjectParameter("diagramname", typeof(string));
+    
+            var owner_idParameter = owner_id.HasValue ?
+                new ObjectParameter("owner_id", owner_id) :
+                new ObjectParameter("owner_id", typeof(int));
+    
+            var new_diagramnameParameter = new_diagramname != null ?
+                new ObjectParameter("new_diagramname", new_diagramname) :
+                new ObjectParameter("new_diagramname", typeof(string));
+    
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction("sp_renamediagram", diagramnameParameter, owner_idParameter, new_diagramnameParameter);
+        }
+    
+        public virtual int sp_upgraddiagrams()
+        {
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction("sp_upgraddiagrams");
+        }
     }
 }
